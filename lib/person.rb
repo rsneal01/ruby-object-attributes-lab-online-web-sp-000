bf20a917f53322014c8999c33e9182f0af26bf3f
Bobby Neal <rsneal01@gmail.com>
2020-05-15 16:45:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,18 @@
 class Person
   
   def name=(person_name)
-    
+    @person = person_name
+  end
+  
+  def name
+    @person
+  end
+  
+  def job=(job_name)
+    @job = job_name
+  end
+  
+  def job
+    @job
+  end
 end
